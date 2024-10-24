79d0625b3da109fe89c5fe97150c60a1b407c198
WinZ606 <2005bnm@gmail.com>
2024-10-24 11:12:01 +0200

    vince

--- etlap.py
+++ etlap.py
@@ -1,5 +1,29 @@
+def csillag1():
+    print("*"*36)
+    print("LEVESEK")
+
+def csillag2():
+    print("*"*38)
+    print("FŐÉTELEK")
+
 def kiir(levesek, levesek_ar):
-    i  =   0
+    i = 0
+    csillag1()
     while i < len(levesek):
-        print(f"{i+1}. {levesek[i]} {levesek_ar[i]}")
+        if len(levesek[i]) > 12:
+            pontok = ("."*7)
+        else:
+            pontok = ("."*12)
+        print(f"{i+1}. -> {levesek[i]} {pontok} {levesek_ar[i]:>.0f} Ft")
+        i += 1
+
+def kiir(levesek, levesek_ar):
+    i = 0
+    csillag1()
+    while i < len(levesek):
+        if len(levesek[i]) > 12:
+            pontok = ("."*7)
+        else:
+            pontok = ("."*12)
+        print(f"{i+1}. -> {levesek[i]} {pontok} {levesek_ar[i]:>.0f} Ft")
         i += 1
--- main.py
+++ main.py
@@ -7,3 +7,4 @@ foetelek = ["Rántotthús", "Töltöttkáposzta", "Pörkölt"]
 foetele_ar = [2000, 1600, 1800]
 
 etlap.kiir(levesek, levesek_ar)
+etlap.kiir(foetelek, foetele_ar)
