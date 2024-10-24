0e1b183acc721549df38f56dbedcff4684e8482d
Barni <tothbarni.06@gmail.com>
2024-10-24 10:28:17 +0200

    alap

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("1.")
-print("második")
-print("eaeaeaeae")
-print("dbjdkjk")
+import nyugta
+
+levesek = ["Húsleves", "Gyümölcsleves", "Bableves"]
+levesek_ar = [1000, 600, 800]
+foetelek = ["Rántotthús", "Töltöttkáposzta", "Pörkölt"]
+foetele_ar = [2000, 1600, 1800]
+
