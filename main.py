bd03263476634bca740de6a389c37757df5442f6
Barni <tothbarni.06@gmail.com>
2024-10-24 10:47:05 +0200

    Működő étlap

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import nyugta
+import etlap
 
 levesek = ["Húsleves", "Gyümölcsleves", "Bableves"]
 levesek_ar = [1000, 600, 800]
 foetelek = ["Rántotthús", "Töltöttkáposzta", "Pörkölt"]
 foetele_ar = [2000, 1600, 1800]
 
+etlap.kiir(levesek, levesek_ar)
