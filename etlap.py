8e067314e6cca9c19a2e9bc382547ff8c73702ca
Barni <tothbarni.06@gmail.com>
2024-10-24 11:12:42 +0200

    Alap etrend

--- etlap.py
+++ etlap.py
@@ -1,5 +1,8 @@
+def kiir2(jel:str = "=", szam:int = 22):
+    print(jel * szam)
+
 def kiir(levesek, levesek_ar):
     i  =   0
     while i < len(levesek):
-        print(f"{i+1}. {levesek[i]} {levesek_ar[i]}")
+        print(f"{i+1}. {levesek[i]} {levesek_ar[i]:>.0f}FT")
         i += 1
