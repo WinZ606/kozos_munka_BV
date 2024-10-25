fa742f6a555ef2a67e3d2d92f57658240b9def04
Dezső Vince <2005bnm@gmail.com>
2024-10-25 13:53:05 +0200

    nincs foetel

--- rendeles.py
+++ rendeles.py
@@ -18,18 +18,18 @@ def rendeles1(levesek,levesek_ar,foetelek,foetelek_ar):
         elif eves == 1:
             foetel = int(input(f"Mit kér?\n"))
             rend_foet.append(foetelek[foetel-1]),rend_foet_ar.append(foetelek_ar[foetel-1])
-            while foetel != 0:
+            while foetel != -1:
                 rend_lev.append(levesek[leves-1]),rend_lev_ar.append(levesek_ar[leves-1])
-                leves = int(input("Még valamit? \n"))
+                leves = int(input(f"Még valamit? \n"))
             print(rend_lev)
             print(rend_lev_ar)
             print(rend_foet)
             print(rend_foet_ar)
-    elif rendeles == 2:
-        rend_foet.append(foetelek[foetel-1]),rend_foet_ar.append(foetelek_ar[foetel-1])
+    else:
         foetel = int(input(f"Mit kér?\n"))
-        while foetel != 0:
-            rend_lev.append(foetel[foetel-1]),rend_foet_ar.append(foetelek_ar[foetel-1])
+        rend_foet.append(foetelek[foetel-1]),rend_foet_ar.append(foetelek_ar[foetel-1])
+        while foetel != -1:
+            rend_lev.append(foetelek[foetel-1]),rend_foet_ar.append(foetelek_ar[foetel-1])
             foetel = int(input("Még valamit? \n"))
         print(rend_foet)
         print(rend_foet_ar)
