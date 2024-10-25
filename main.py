116fb9780c4dee2664d27e867ddc3ebd3543617b
Dezső Vince András (Szf_N_2024_2026) <dezso.vince.andras@diak.szamalk-szalezi.hu>
2024-10-25 12:56:36 +0200

    rendeles nem megy

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ foetele_ar = [2000, 1600, 1800]
 
 etlap.kiir2(); etlap.kiir2("|           LEVESEK:         |", 1); etlap.kiir2()
 etlap.kiir(levesek, levesek_ar)
-etlap.kiir2(); etlap.kiir2("|      FŐÉTELEK:        |", 1); etlap.kiir2()
+etlap.kiir2(); etlap.kiir2("|           FŐÉTELEK:        |", 1); etlap.kiir2()
 etlap.kiir(foetelek, foetele_ar)
-rendeles.rendeles1()
+etlap.kiir2(); etlap.kiir2("|           RENDELÉS:        |", 1); etlap.kiir2()
+rendeles.rendeles1(levesek,levesek_ar,foetelek,foetele_ar)
--- rendeles.py
+++ rendeles.py
@@ -1,8 +1,24 @@
 import etlap
 
-def rendeles1(lev1,lev_ar,foet,foet_ar):
-    rendeles = int(input(f"Mit kér?\n1 -> Levesek\n2 -> Főételek"))
+def rendeles1(levesek,levesek_ar,foetelek,foetelek_ar):
+    rend_lev = []
+    rend_lev_ar = []
+    rend_foet = []
+    rend_foet_ar = []
+    rendeles = int(input(f"1. Levesek\n2. Főételek\nMit kér? "))
     if rendeles == 1:
-        leves = int(input(f"Itt vannak a leveseink: \n"))
-    else:
-        print()
+        leves = int(input(f"Mit kér?\n"))
+        rend_lev.append(levesek[leves]),rend_lev_ar.append(levesek_ar[leves])
+        while leves >=1 and leves <= 3:
+            leves == int(input("Még valamit? \n"))
+            rend_lev.append(levesek[leves]),rend_lev_ar.append(levesek_ar[leves])
+    elif rendeles == 2:
+        foetel = int(input(f"Mit kér?\n"))
+        rend_foet.append(foetelek[foetel]),rend_foet_ar.append(foetelek_ar[foetel])
+        while leves != 0:
+            foetel == int(input("Még valamit? \n"))
+            rend_lev.append(foetel[foetel]),rend_foet_ar.append(foetelek_ar[foetel])
+    print(rend_lev)
+    print(rend_lev_ar)
+    print(rend_foet)
+    print(rend_foet_ar)
