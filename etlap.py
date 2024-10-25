676deea94839884935fbec56984a6e83fd46d8ba
Barni <tothbarni.06@gmail.com>
2024-10-25 12:18:53 +0200

    mukodo alap

--- etlap.py
+++ etlap.py
@@ -1,8 +1,8 @@
-def kiir2(jel:str = "=", szam:int = 25):
+def kiir2(jel:str = "=", szam:int = 30):
     print(jel * szam)
 
-def kiir(levesek, levesek_ar):
+def kiir(levesek, levesek_ar, hossz:int=30):
     i = 0
     while i < len(levesek):
-        print(f"{i+1}. {levesek[i]} {levesek_ar[i]:>.0f}FT")
+        print(f"{i + 1}. {levesek[i]} {levesek_ar[i]:>5.0f} FT")
         i += 1
