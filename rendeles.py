a6034e7f3be546ccc7cc76ef5d6cc78b65d46ea5
Dezső Vince <2005bnm@gmail.com>
2024-10-25 13:25:22 +0200

    egyeztetes

--- rendeles.py
+++ rendeles.py
@@ -9,15 +9,15 @@ def rendeles1(levesek,levesek_ar,foetelek,foetelek_ar):
     if rendeles == 1:
         leves = int(input(f"Mit kér?\n"))
         rend_lev.append(levesek[leves]),rend_lev_ar.append(levesek_ar[leves])
-        while leves >=1 and leves <= 3:
-            leves == int(input("Még valamit? \n"))
-            rend_lev.append(levesek[leves]),rend_lev_ar.append(levesek_ar[leves])
-    elif rendeles == 2:
-        foetel = int(input(f"Mit kér?\n"))
-        rend_foet.append(foetelek[foetel]),rend_foet_ar.append(foetelek_ar[foetel])
         while leves != 0:
-            foetel == int(input("Még valamit? \n"))
+            rend_lev.append(levesek[leves]),rend_lev_ar.append(levesek_ar[leves])
+            leves = int(input("Még valamit? \n"))
+    elif rendeles == 2:
+        rend_foet.append(foetelek[foetel]),rend_foet_ar.append(foetelek_ar[foetel])
+        foetel = int(input(f"Mit kér?\n"))
+        while foetel != 0:
             rend_lev.append(foetel[foetel]),rend_foet_ar.append(foetelek_ar[foetel])
+            foetel = int(input("Még valamit? \n"))
     print(rend_lev)
     print(rend_lev_ar)
     print(rend_foet)
